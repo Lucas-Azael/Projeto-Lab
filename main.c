70e336d489d97322c9ff4f0ae6d3fee2a47e5126
Lucas Soares <azaelsoares@gmail.com>
2019-10-28 20:24:22 -0300

    adicionando structs

--- main.c
+++ main.c
@@ -1,41 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+typedef struct {
+    int capacidade;
+    char local[30];
+    int cargaHoraria;
+    char tema[80];
+    char horario[20];
+}Palestra;
 
+typedef struct {
+    int capacidade;
+    char local[30];
+    int cargaHoraria;
+    char tema[80];
+    char horario[20];
+}Oficina;
 
-typedef struct
-{
-    char nome[30];
-    char palestra[30];
-    char oficina[30];
-    char grupoDiscussao[30];
-    char cursos[30];
-}Congressista;
+typedef struct {
+    int capacidade;
+    char local[30];
+    int cargaHoraria;
+    char tema[80];
+    char horario[20];
+}GpDiscussao;
 
-Congressista setCongressista(char nome[30], char palestra[30], char oficina[30], char grpDisc[30],char cursos[30], Congressista C){
-    C.nome[30] = nome[30];
-    C.palestra[30] = palestra[30];
-    C.oficina[30] = oficina[30];
-    C.grupoDiscussao[30] = grpDisc[30];
-    C.cursos[30] = cursos[30];
-    return C;
+typedef struct {
+    int capacidade;
+    char local[30];
+    int cargaHoraria;
+    char tema[80];
+    char horario[20];
+}Curso;
+
+typedef struct {
+    int capacidade;
+    char local[30];
+    int cargaHoraria;
+    char tema[80];
+    char horario[20];
+}Oficina;
+
+void cadastrarPalestra(Palestra *p) {
+    setbuf(stdin, NULL);
+    printf("Digite o local\n");
+    gets(p->local);
+    printf("Carga horária: ");
+    gets(p->cargaHoraria);
+    printf("Tema: ");
+    gets(p->tema);
+    printf("Horário: ");
+    gets(p->horario);
 }
 
-void armazenaCongressista(Congressista C, int n) {
-    Congressista qtd[300];
-    int i;
-    for(i=0;i<300;i++){
-        qtd[n] = C;
-    }
-}
 
 int main() {
     
-    char nome[30];
-    char palestra[30];
-    char oficina[30];
-    char grupoDiscussao[30];
-    char curso[30];
-    Congressista C;
-
+    
+    setlocale(LC_ALL, "Portuguese");
     int i;
 
     printf("Digite 1 para cadastrar congressista \n");
@@ -44,19 +67,7 @@ int main() {
     switch (i)
     {
     case 1:
-        printf("Nome: ");
-        fgets(nome, 30, stdin);
-        printf("Palestra: ");
-        fgets(palestra, 30, stdin);
-        printf("Oficina: ");
-        fgets(oficina, 30, stdin);
-        printf("Grupo de Discussao: ");
-        fgets(grupoDiscussao, 30, stdin);
-        printf("Nome: ");
-        fgets(curso, 30, stdin);
-        setCongressista(nome, palestra, oficina, grupoDiscussao, curso, C);
-
-        printf("Nome: %c // palestra: %c\n", C.nome, C.palestra);
+        
 
         break;
     
